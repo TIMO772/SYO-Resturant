b462467c26ac964790798002d99a359f1642b782
TIMO772 <timmahameed33@gmail.com>
2023-11-25 00:34:57 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,12 @@
+let About = document.querySelector('#About')
+let Menu = document.querySelector('#Menu')
+let Team = document.querySelector('#Team')
+About.addEventListener('click', () => {
+    window.scrollTo(0,593.953125)
+})
+Menu.addEventListener('click', () => {
+    window.scrollTo(0,1356.24462890625)
+})
+Team.addEventListener('click', () => {
+    window.scrollTo(0,4364)
+})
